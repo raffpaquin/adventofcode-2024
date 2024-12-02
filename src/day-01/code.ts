27b345a6c42af72fe8f161031057e83fb8844282
Raff Paquin <me@raff.ca>
2024-12-02 10:08:00 -0500

    feat: adds biome

--- src/day-01/code.ts
+++ src/day-01/code.ts
@@ -1,12 +1,9 @@
 export const add = (numbers: string): number => {
-  let integers = numbers.split(',').map(x => parseInt(x));
-  let negatives = integers.filter(x => x < 0);
+  const integers = numbers.split(',').map((x) => Number.parseInt(x));
+  const negatives = integers.filter((x) => x < 0);
 
   if (negatives.length > 0)
-      throw new RangeError('Negatives are not allowed: ' + negatives.join(', '));
+    throw new RangeError('Negatives are not allowed: ' + negatives.join(', '));
 
-  return integers
-      .filter(x => x <= 1000)
-      .reduce((a, b) => a + b, 0);
-}
-
+  return integers.filter((x) => x <= 1000).reduce((a, b) => a + b, 0);
+};
